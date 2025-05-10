530b4a302d294d9b6ce75b0e980fad4ba6ca4db4
rstey <rstega780@gmail.com>
2025-05-10 20:15:05 +0300

    footer header

--- netflix/src/Header/Header.js
+++ netflix/src/Header/Header.js
@@ -1,11 +1,37 @@
 import React from 'react'
 
+import './Header.css'
+import Netflix from './10001.png'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 function Header() {
   return (
-    <div>
+    <div class ="header-outer-container">
+        <div className="header-container">
+            <div className='header-left'>
+                <ul className='header-list'>
+                    <li className='header-list-item'><img src={Netflix}alt="Netflix logo " width="100"/></li>
+                    <li className='header-list-item'>Home</li>
+                    <li className='header-list-item'>TV Shows</li>
+                    <li className='header-list-item'>Movies</li>
+                    <li className='header-list-item'>Latest</li>
+                    <li className='header-list-item'>My List</li>
+                </ul>
+        </div>
+            <div className='header-right'>
+                <ul className='header-list'>
+                    <li className='header-list-item'><SearchIcon /></li>
+                    <li className='header-list-item'><NotificationsIcon /></li>
+                    <li className='header-list-item'><AccountCircleIcon /></li>
+                </ul>
+            </div>
        
 
     </div>
+    </div>
+
 
   )
 
--- netflix/src/requests.js
+++ netflix/src/requests.js
@@ -2,6 +2,7 @@
 const API_KEY = "4a67bdabb180a83b3e409ba8d12eaedf"; // Your API key
 const BASE_URL = "https://api.themoviedb.org/3";
 
+
 const requests = {
   fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=28`,
